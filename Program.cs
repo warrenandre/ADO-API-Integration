58e3fa6c9a0afc6d1ffb504eebc035d1995397b9
Warren Joubert <wjoubert@microsoft.com>
2023-08-08 20:19:37 +0200

    updates to get item counts and write out to csv file

--- Program.cs
+++ Program.cs
@@ -6,10 +6,13 @@ using Microsoft.TeamFoundation.Build.WebApi;
 using Microsoft.TeamFoundation.Common;
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
+
+//class to hold project details
 
 var PAT = "";
 var orgName = "";
@@ -26,24 +29,23 @@ var projects = await GetProjects(PAT, orgName, DateTime.Now.AddMonths(-int.Parse
 
 //write out projects resuls to csv file
 var csv = new StringBuilder();
-csv.AppendLine("Project Name,URL");
+csv.AppendLine("Project Name,Work Items Updated,Total Work Items,Builds,Repo Updates,Test Plans updates");
 
 foreach (var project in projects)
 {
-    csv.AppendLine($"{project.Name},{project.Url}");
-    Console.WriteLine($"{project.Name}");
+    csv.AppendLine($"{project.ProjectName},{project.WorkItemCount},{project.TotalWorkItemCount},{project.BuildCount},{project.RepoCount},{project.TestPlanCount}");
+    Console.WriteLine($"{project.ProjectName}");
 }
 csv.AppendLine(
-    $"Total Projects,{projects.Count}");
+    $"Total Projects : {projects.Count}");
 File.WriteAllText("projects.csv", csv.ToString());
 
 
 
-
 //method to get all projects from azure devops
-static async Task<List<TeamProjectReference>> GetProjects(string pat, string orgName, DateTime dateTime)
+static async Task<List<ProjecDetails>> GetProjects(string pat, string orgName, DateTime dateTime)
 {
-    var activeProjects = new List<TeamProjectReference>();
+    var activeProjects = new List<ProjecDetails>();
     try
     {
 
@@ -53,30 +55,19 @@ static async Task<List<TeamProjectReference>> GetProjects(string pat, string org
         foreach (var project in projects.Where(p => p.State != ProjectState.Deleted))
         {
             //check work items
-            var checkWorkItems = await GetProjectWorkItems(pat, orgName, project.Name, dateTime);
-            if (checkWorkItems)
+            var workItemCount = await GetProjectWorkItems(pat, orgName, project.Name, dateTime);
+            var totalWorkItemsCount = await GetTotalWorkItems(pat, orgName, project.Name);
+            var totalBuilds = await GetProjectBuilds(pat, orgName, project.Name, dateTime);
+            var repoCount = await GetProjectRepos(pat, orgName, project.Name, dateTime);
+            var testPlansCount = await GetProjectTestPlans(pat, orgName, project.Name, dateTime);
+            if(totalBuilds > 0 || workItemCount > 0 || repoCount > 0 || totalWorkItemsCount > 0)
             {
-                activeProjects.Add(project);
-                continue;
+                //create projectdetails object
+                var projectDetails = new ProjectDetails(project.Name, workItemCount, totalWorkItemsCount, totalBuilds, repoCount, testPlansCount);
+                activeProjects.Add(projectDetails);
+                
             }
-
-            // check builds
-            var checkBuilds = await GetProjectBuilds(pat, orgName, project.Name, dateTime);
-            if (checkBuilds)
-            {
-                activeProjects.Add(project);
-                continue;
-            }
-
-            // check repos
-            var checkRepos = await GetProjectRepos(pat, orgName, project.Name, dateTime);
-            if (checkRepos)
-            {
-                activeProjects.Add(project);
-                continue;
-            }
-
-            //perhaps add test plans
+           
 
         }
     }catch(Exception ex)
@@ -87,9 +78,23 @@ static async Task<List<TeamProjectReference>> GetProjects(string pat, string org
     return activeProjects;
 }
 
+//method to get project testplans
+static async Task<int> GetProjectTestPlans(string pat, string orgName, string projectname, DateTime dateTime)
+{
+    var testClient = await GetTestClient(pat, orgName);
+    var testRuns = await testClient.GetTestRunsAsync(projectname);
+    var testPlanCount = 0;
+    foreach (var testPlan in testRuns)
+    {
+        //get last update date for test plan
+        if (testPlan.CompletedDate >= dateTime)
+            testPlanCount++;
+    }
+    return testPlanCount;
+}
 
 //method to get project work items
-static async Task<bool> GetProjectWorkItems(string pat, string orgName, string projectname, DateTime dateTime)
+static async Task<int> GetProjectWorkItems(string pat, string orgName, string projectname, DateTime dateTime)
 {
     var workItemClient = await GetWorkItemClient(pat, orgName);
     var workItems = new List<WorkItem>();
@@ -97,37 +102,53 @@ static async Task<bool> GetProjectWorkItems(string pat, string orgName, string p
     var wiql = new Wiql()
     {
         Query = $"SELECT [System.Id] FROM WorkItems WHERE [System.TeamProject] = '{projectname}' AND [System.ChangedDate] > '{iso8601}' ORDER BY [System.ChangedDate] DESC"
-
-        
+ 
     };
     var result = await workItemClient.QueryByWiqlAsync(wiql).ConfigureAwait(false);
     //if work item update date older than 2 months then add to list
-    var ids = result.WorkItems.Select(item => item.Id).ToArray();
+    var ids = result.WorkItems.Select(item => item.Id).Count();
 
-    return ids.Any();
+    return ids;
+}
+
+static async Task<int> GetTotalWorkItems(string pat, string orgName, string projectname)
+{
+    var workItemClient = await GetWorkItemClient(pat, orgName);
+    var workItems = new List<WorkItem>();
+    var wiql = new Wiql()
+    {
+        Query = $"SELECT [System.Id] FROM WorkItems WHERE [System.TeamProject] = '{projectname}' ORDER BY [System.ChangedDate] DESC"
+ 
+    };
+    var result = await workItemClient.QueryByWiqlAsync(wiql).ConfigureAwait(false);
+    //if work item update date older than 2 months then add to list
+    var ids = result.WorkItems.Select(item => item.Id).Count();
+
+    return ids;
 }
 
 //get project repositories
-static async Task<bool> GetProjectRepos(string pat, string orgName, string projectname, DateTime dateTime)
+static async Task<int> GetProjectRepos(string pat, string orgName, string projectname, DateTime dateTime)
 {
     var gitClient = await GetGitClient(pat, orgName);
     var repos = await gitClient.GetRepositoriesAsync(projectname);
+    var repoCount = 0;
     foreach (var repo in repos)
     {
         //get last update date for repository
         if (repo.ProjectReference.LastUpdateTime >= dateTime)
-            return true;
+            repoCount++;
     }
-    return false;
+    return repoCount;
 }
 
 
 //get project Builds
-static async Task<bool> GetProjectBuilds(string pat, string orgName, string projectname, DateTime dateTime)
+static async Task<int> GetProjectBuilds(string pat, string orgName, string projectname, DateTime dateTime)
 {
     var buildClient = await GetBuildClient(pat, orgName);
     var builds = await buildClient.GetBuildsAsync(projectname);
-    return builds.Any(x => x.LastChangedDate >= dateTime);
+    return builds.Where(x => x.LastChangedDate >= dateTime).Count();
     
 }
 
@@ -181,6 +202,17 @@ static async Task<ProjectHttpClient> GetProjectClient(string pat, string orgName
     return projectClient;
 }
 
+//create testitemsclient method
+static async Task<TestManagementHttpClient> GetTestClient(string pat, string orgName)
+{
+    // create a connection
+    VssConnection connection = new VssConnection(new Uri($"https://dev.azure.com/{orgName}"), new VssBasicCredential(string.Empty, pat));
+
+    // create client
+    TestManagementHttpClient testClient = connection.GetClient<TestManagementHttpClient>();
+
+    return testClient;
+}
 
 //create GetWorkItemClient method
 static async Task<WorkItemTrackingHttpClient> GetWorkItemClient(string pat, string orgName)
@@ -193,3 +225,28 @@ static async Task<WorkItemTrackingHttpClient> GetWorkItemClient(string pat, stri
 
     return workItemClient;
 }
+
+
+internal class ProjecDetails
+{
+    public string ProjectName { get; set; }
+    public int WorkItemCount { get; set; }
+    public int BuildCount { get; set; }
+    public int RepoCount { get; set; }
+    public int TestPlanCount { get; set; }
+    public int TotalWorkItemCount { get; set; }
+    public int TestPlansCount { get; set; }
+
+    //constructor
+    // public ProjecDetails(string projectName, int workItemCount, int buildCount, int repoCount, int testPlanCount, int totalWorkItemCount, int testPlansCount){
+    //     ProjectName = projectName;
+    //     WorkItemCount = workItemCount;
+    //     BuildCount = buildCount;
+    //     RepoCount = repoCount;
+    //     TestPlanCount = testPlanCount;
+    //     TotalWorkItemCount = totalWorkItemCount;
+    //     TestPlansCount = testPlansCount;
+    // }
+}
+
+
---
+++ ProjectDetails.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.TeamFoundation.Core.WebApi;
+
+internal class ProjectDetails : ProjecDetails
+{
+
+    public ProjectDetails(string project, int workItemCount, int totalWorkItemsCount, int totalBuilds, int repoCount, int testPlansCount)
+    {
+        this.ProjectName = project;
+        WorkItemCount = workItemCount;
+        this.TotalWorkItemCount = totalWorkItemsCount;
+        this.BuildCount = totalBuilds;
+        RepoCount = repoCount;
+        TestPlansCount = testPlansCount;
+    }
+}
